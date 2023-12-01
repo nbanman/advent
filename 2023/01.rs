29cf36c88cbdd4239c52273536b0d74dff4aa45d
Neil Banman <neil.banman@gmail.com>
2023-12-01 13:32:51 -0800

    miserable attempt...but it works

--- 2023/01.rs
+++ 2023/01.rs
@@ -4,12 +4,66 @@ fn default_input() -> &'static str {
     include_input!(2023 / 01)
 }
 
-fn part1(calibration: &str) -> i64 {
-    3
+fn part1(calibration: &str) -> u32 {
+    calibration
+        .lines()
+        .map(|line| {
+            let first = line
+                .chars()
+                .find(|c| c.is_digit(10))
+                .unwrap()
+                .to_digit(10)
+                .unwrap();
+            let second = line
+                .chars()
+                .rfind(|c| c.is_digit(10))
+                .unwrap()
+                .to_digit(10)
+                .unwrap();
+            first * 10 + second
+        }).sum()
 }
 
-fn part2(calibration: &str) -> i64 {
-    3
+fn part2(calibration: &str) -> u32 {
+    let pattern = regex!(r"(\d|one|two|three|four|five|six|seven|eight|nine)");
+    let reverse_pattern = regex!(r"(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)");
+    calibration
+        .lines()
+        .map(|line| {
+            let first = pattern
+                .captures(line)
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .as_str();
+            let reversed_line = line.chars().rev().collect::<String>();
+            // println!("{}", reversed_line);
+            
+            let second = reverse_pattern
+                .captures(&reversed_line)
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .as_str();
+            let value = str_to_u32(first) * 10 + str_to_u32(second);
+            println!("line: {}, first: {}, second: {}, value: {}", line, first, second, value);
+            value
+        }).sum()
+}
+
+fn str_to_u32(s: &str) -> u32 {
+    match s {
+        "one" | "eno" => 1,
+        "two" | "owt" => 2,
+        "three" | "eerht" => 3,
+        "four" | "ruof" => 4,
+        "five" | "evif" => 5,
+        "six" | "xis" => 6,
+        "seven" | "neves" => 7,
+        "eight" | "thgie" => 8,
+        "nine" | "enin" => 9,
+        _ => s.chars().next().unwrap().to_digit(10).unwrap(),
+    }
 }
 
 fn main() {
