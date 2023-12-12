eb0fd2d9c15f1866f13fd42f1b1be67662f5042f
Neil Banman <neil.banman@gmail.com>
2023-12-11 16:26:49 -0800

    added comments

--- 2023/11.rs
+++ 2023/11.rs
@@ -11,6 +11,30 @@ struct Image {
 fn parse_input(data: &str) -> Image {
     let width = data.find('\n').unwrap() + 1;
     let height = data.len() / width;
+
+    // for each axis, get indexed iterables of pairs. The first being the index where galaxies reside, the
+    // second being the number of galaxies. We separate the axes to avoid repeat calculation.
+    let x_galaxies: Vec<(usize, usize)> = (0..width).into_iter()
+        .map(|x| {
+            let count = (0..height).map(|y| data.chars().nth(x + y * width).unwrap())
+                .filter(|c| *c == '#')
+                .count();
+            (x, count)
+        })
+        .filter(|(_, count)| *count > 0)
+        .collect();
+
+    // for each axis, track the indices representing expansion fields
+    let y_galaxies: Vec<(usize, usize)> = (0..height).into_iter()
+        .map(|y| {
+            let count = &data[y * width .. y * width + width].chars()
+                .filter(|c| *c == '#')
+                .count();
+            (y, *count)
+        })
+        .filter(|(_, count)| *count > 0)
+        .collect();
+
     let x_expansion: Vec<usize> = (0..width - 1).into_iter()
         .filter(|x| {
             (0..height).into_iter().all(|y| data.chars().nth(*x + y * width).unwrap() == '.')
@@ -24,32 +48,21 @@ fn parse_input(data: &str) -> Image {
         })
         .collect();
 
-    let y_galaxies: Vec<(usize, usize)> = (0..height).into_iter()
-        .map(|y| {
-            let count = &data[y * width .. y * width + width].chars()
-                .filter(|c| *c == '#')
-                .count();
-            (y, *count)
-        })
-        .filter(|(_, count)| *count > 0)
-        .collect();
-
-    let x_galaxies: Vec<(usize, usize)> = (0..width).into_iter()
-        .map(|x| {
-            let count = (0..height).map(|y| data.chars().nth(x + y * width).unwrap())
-                .filter(|c| *c == '#')
-                .count();
-            (x, count)
-        })
-        .filter(|(_, count)| *count > 0)
-        .collect();
-
     Image { x_expansions: x_expansion, y_expansions: y_expansion, x_galaxies, y_galaxies }
 }
 
-fn default_input() -> Image { parse_input(include_input!(2023 / 11))
+fn default_input() -> Image { parse_input(include_input!(2023 / 11)) }
+
+// run the distance function twice (once for each axis), return the sum
+fn solve(image: &Image, expansion_factor: usize) -> usize {
+    distance(expansion_factor, &image.x_galaxies, &image.x_expansions) +
+        distance(expansion_factor, &image.y_galaxies, &image.y_expansions)
 }
 
+// Get the distance between two indices where galaxies reside.
+// This involves calculating the unexpanded difference multiplied by the number of expansions passed times
+// the expansion factor
+// Lastly, the distance is multiplied by #galaxies in the first index multiplied by #galaxies in the second index.
 fn distance(
     expansion_factor: usize,
     galaxies: &Vec<(usize, usize)>,
@@ -57,9 +70,17 @@ fn distance(
 ) -> usize {
     galaxies.iter().enumerate()
         .map(|(i, (a_pos, a_count))| {
+
+
+            // calculate which expansions are to the left of the source galaxies
+            // this returns a negative number due to how binarySearch returns values but this will be rectified
+            // later.
             let already_passed = expansions.binary_search(a_pos).err().unwrap();
             galaxies.iter().skip(i + 1)
                 .map(|(b_pos, b_count)| {
+
+                    // calculates which expansions are to the left of the destination galaxies, and subtracts the
+                    // ones to the left of the source galaxies. The abs function handles the negative value.
                     let expansions_passed = expansions.binary_search(b_pos).err().unwrap()
                         - already_passed;
                     ((b_pos - a_pos) + expansions_passed * (expansion_factor - 1)) * a_count * b_count
@@ -69,10 +90,6 @@ fn distance(
         .sum()
 }
 
-fn solve(image: &Image, expansion_factor: usize) -> usize {
-    distance(expansion_factor, &image.x_galaxies, &image.x_expansions) +
-        distance(expansion_factor, &image.y_galaxies, &image.y_expansions)
-}
 fn part1(image: Image) -> usize {
     solve(&image, 2)
 }
