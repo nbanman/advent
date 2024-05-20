bbd795dc4a1bebd0839d0e2fe4329e29bf97aaf3
Neil Banman <neil.banman@gmail.com>
2024-05-20 00:38:32 -0700

    initial, slow!

--- 2022/14.rs
+++ 2022/14.rs
@@ -1,68 +1,68 @@
 use advent::prelude::*;
-
-fn parse_input(input: &str) -> HashSet<Vector2> {
-    input
+fn parse_input(input: &str) -> (HashSet<Vector2>, i64) {
+    let cavern: HashSet<Vector2> = input
         .lines()
         .flat_map(|line| {
-            line.split(" -> ")
-                .map(|s| {
-                    s.split(',')
-                        .map(str::parse)
-                        .map(Result::unwrap)
+            line
+                .split(" -> ")
+                .filter_map(|pt_str| {
+                    pt_str
+                        .split(',')
+                        .filter_map(|xy| xy.parse().ok())
                         .next_array()
                         .map(Vector2::from)
-                        .unwrap()
                 })
                 .array_windows()
                 .flat_map(|[p1, p2]| {
-                    let d = (p2 - p1).map(i64::signum);
-                    iter::successors(Some(p1), move |&p| (p != p2).some(p + d))
+                    let diff = (p2 - p1).map(i64::signum);
+                    iter::successors(Some(p1), move |&p| (p != p2).some(p + diff))
                 })
         })
-        .collect()
+        .collect();
+
+    let depth = cavern.iter().map(|v| v.y).max().unwrap();
+
+    (cavern, depth)
 }
 
-fn default_input() -> HashSet<Vector2> {
+fn default_input() -> (HashSet<Vector2>, i64) {
     parse_input(include_input!(2022 / 14))
 }
 
-fn solve(mut cave: HashSet<Vector2>, part2: bool) -> usize {
-    let mut max_y = cave.iter().map(|v| v.y).max().unwrap();
-    if part2 {
-        max_y += 2;
+fn solve<F>(cavern: &mut HashSet<Vector2>, depth: i64, predicate: F) -> usize
+where
+    F: Fn(Vector2) -> bool
+{
+    let mut index = 0usize;
+    loop {
+        let grain = settle(cavern, depth);
+        if predicate(grain) { return index }
+        cavern.insert(grain);
+        index += 1;
     }
-    let mut flowing = vec![vector![500, 0]];
-    let mut count = 0;
-    while let Some(sand) = flowing.last().copied() {
-        // Check if there is air in any of the following directions
-        match vectors!([0, 1], [-1, 1], [1, 1])
-            .into_iter()
-            .map(|d| sand + d)
-            .find_map(|p| (!cave.contains(&p)).some(p))
-        {
-            // There is space for the sand to flow, so update the sand position
-            // and continue flowing
-            Some(p) if p.y < max_y => flowing.push(p),
-            // We've gone out of bounds, if part 1 then we're done
-            Some(_) if !part2 => break,
-            // Otherwise we can no longer flow, so we just insert sand at this
-            // location
-            _ => {
-                cave.insert(sand);
-                flowing.pop();
-                count += 1;
-            }
-        }
-    }
-    count
 }
 
-fn part1(cave: HashSet<Vector2>) -> usize {
-    solve(cave, false)
+fn fall(cavern: &HashSet<Vector2>, grain: &Vector2) -> Option<Vector2> {
+    vec![vector![0, 1], vector![-1, 1], vector![1, 1]]
+        .into_iter()
+        .map(|v| v + grain)
+        .find(|v| !cavern.contains(v))
 }
 
-fn part2(cave: HashSet<Vector2>) -> usize {
-    solve(cave, true)
+fn settle(cavern: &HashSet<Vector2>, depth: i64) -> Vector2 {
+    iter::successors(Some(vector![500, 0]), |grain| fall(cavern, grain))
+        .take_while(|grain| grain.y <= depth + 1)
+        .last()
+        .unwrap()
+}
+
+fn part1((mut cavern, depth): (HashSet<Vector2>, i64)) -> usize {
+    solve(&mut cavern, depth, |v| v.y > depth)
+}
+
+fn part2((mut cavern, depth): (HashSet<Vector2>, i64)) -> usize {
+    let top = vector!(500, 0);
+    solve(&mut cavern, depth, |v| v == top) + 1
 }
 
 fn main() {
@@ -83,6 +83,6 @@ fn example() {
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(input.clone()), 774);
-    assert_eq!(part2(input), 22499);
+    assert_eq!(part1(input.clone()), 825);
+    assert_eq!(part2(input), 26729);
 }
