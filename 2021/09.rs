b28bd3f5296e7d04888b7d5c3d789c0c15d34649
Neil Banman <neil.banman@gmail.com>
2024-05-29 11:06:03 -0700

    initial

--- 2021/09.rs
+++ 2021/09.rs
@@ -1,54 +1,84 @@
 use advent::prelude::*;
 
-fn parse_input(input: &str) -> HashMap<Vector2, i64> {
-    parse_map(input, |c| match c {
-        c @ '0'..='9' => c as i64 - b'0' as i64,
-        c => panic!("unexpected character `{c}`"),
-    })
+fn parse_input(input: &str) -> (Vec<u8>, Vec<isize>, Vec<usize>) {
+    let width = input.as_bytes().iter().position(|&x| x == b'\n').unwrap() as isize + 1;
+
+    let height_map: Vec<u8> = input
+        .as_bytes()
+        .iter()
+        .map(|&c| {
+            if c == b'\n' {
+                c
+            } else {
+                c - b'0'
+            }
+        })
+        .collect();
+
+    let start_vec = vec![-width, width, -1, 1];
+
+    let low_indices = (0..height_map.len())
+        .filter(|&idx| {
+            start_vec
+                .iter()
+                .map(|m| idx as isize + m)
+                .filter_map(|neighbor_idx| {
+                    height_map
+                        .try_get(neighbor_idx)
+                        .and_then(|x| if x == &b'\n' { None } else { Some(x) })
+                })
+                .all(|neighbor| neighbor > height_map.get(idx).unwrap())
+        })
+        .collect();
+    (height_map, start_vec, low_indices)
 }
 
-fn default_input() -> HashMap<Vector2, i64> {
+fn default_input() -> (Vec<u8>, Vec<isize>, Vec<usize>) {
     parse_input(include_input!(2021 / 09))
 }
 
-const CARDINALS: [Vector2; 4] = vectors!([0, -1], [0, 1], [-1, 0], [1, 0]);
-
-fn low_points(map: &HashMap<Vector2, i64>) -> impl Iterator<Item = (Vector2, i64)> + '_ {
-    map.iter()
-        .filter(|(p, height)| {
-            CARDINALS
-                .iter()
-                .filter_map(|d| map.get(&(*p + d)))
-                .all(|h| h > height)
-        })
-        .map(|(p, h)| (*p, *h))
+fn part1((height_map, _, low_indices): (Vec<u8>, Vec<isize>, Vec<usize>)) -> usize {
+    low_indices.into_iter()
+        .map(|idx| *height_map.get(idx).unwrap() as usize + 1)
+        .sum()
 }
 
-fn part1(map: HashMap<Vector2, i64>) -> i64 {
-    low_points(&map).map(|(_, h)| h + 1).sum()
-}
+fn part2((height_map, start_vec, low_indices): (Vec<u8>, Vec<isize>, Vec<usize>)) -> usize {
+    low_indices.into_iter()
+        .map(|idx| {
+            let mut q = VecDeque::new();
+            let mut visited = HashSet::new();
+            q.push_front(idx);
+            visited.insert(idx);
+            while !q.is_empty() {
+                let current_idx = q.pop_back().unwrap();
+                let current_height = height_map.get(current_idx).unwrap();
+                let neighbors = start_vec
+                    .iter()
+                    .map(|m| current_idx as isize + m)
+                    .filter(|&neighbor_idx| {
+                        if let Some(neighbor_height) = height_map
+                            .try_get(neighbor_idx)
+                            .and_then(|x| if x == &b'\n' { None } else { Some(x) }) {
 
-fn part2(map: HashMap<Vector2, i64>) -> usize {
-    let mut basins = Vec::new();
-    for (p, _) in low_points(&map) {
-        let mut q = VecDeque::from([p]);
-        let mut visited = HashSet::new();
-        while let Some(p) = q.pop_front() {
-            if visited.contains(&p) {
-                continue;
-            }
-            visited.insert(p);
-            for d in CARDINALS {
-                let next = p + d;
-                if let Some(0..=8) = map.get(&next) {
-                    q.push_back(next);
+                            neighbor_height != &9 && neighbor_height > current_height
+                        } else {
+                            false
+                        }
+                    });
+                for neighbor_idx in neighbors {
+                    let neighbor_idx = neighbor_idx as usize;
+                    if visited.insert(neighbor_idx) {
+                        q.push_front(neighbor_idx);
+                    }
                 }
             }
-        }
-        basins.push(visited.len());
-    }
-    basins.sort_unstable();
-    basins.into_iter().rev().take(3).product()
+            visited.len()
+        })
+        .sorted_by(|a, b| b.cmp(a))
+        .take(3)
+        .reduce(|acc, item| acc * item)
+        .unwrap()
 }
 
 fn main() {
@@ -73,6 +103,6 @@ fn example() {
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(input.clone()), 423);
-    assert_eq!(part2(input), 1198704);
+    assert_eq!(part1(input.clone()), 448);
+    assert_eq!(part2(input), 1417248);
 }
