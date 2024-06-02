e455f3622769eaacac4165daa37e5e21f433cd2c
Neil Banman <neil.banman@gmail.com>
2024-06-01 19:03:46 -0700

    initial

--- 2020/01.rs
+++ 2020/01.rs
@@ -1,43 +1,31 @@
 use advent::prelude::*;
 
-const SUM: i64 = 2020;
-
-fn default_input() -> Vec<i64> {
-    include_input!(2020 / 01)
-        .lines()
-        .map(str::parse)
-        .map(Result::unwrap)
-        .sorted_unstable()
-        .collect()
+fn default_input() -> (Vec<i64>, HashSet<i64>) {
+    let entries = get_numbers(include_input!(2020 / 01));
+    let entry_set: HashSet<_> = entries.iter().map(|n| *n).collect();
+    (entries, entry_set)
 }
 
-fn solve(numbers: &[i64], mut i: usize, c: i64) -> Option<i64> {
-    let mut j = numbers.len() - 1;
-    while i < j {
-        let a = numbers[i];
-        let b = numbers[j];
-        let sum = a + b + c;
-        match sum.cmp(&SUM) {
-            Ordering::Less => i += 1,
-            Ordering::Greater => j -= 1,
-            Ordering::Equal => return Some(a * b),
+fn part1((_, entry_set): (Vec<i64>, HashSet<i64>)) -> i64 {
+    for entry in entry_set.iter() {
+        let complement = 2020 - entry;
+        if entry_set.contains(&complement) {
+            return entry * complement
         }
     }
-    None
+    -1
 }
 
-fn part1(numbers: Vec<i64>) -> i64 {
-    solve(&numbers, 0, 0).unwrap()
-}
+fn part2((entries, entry_set): (Vec<i64>, HashSet<i64>)) -> i64 {
+    for (first, second) in entries.iter().dropping_back(1)
+        .cartesian_product(entries.iter().dropping(1)) {
 
-fn part2(numbers: Vec<i64>) -> i64 {
-    for i in 0..numbers.len() - 2 {
-        let c = numbers[i];
-        if let Some(ab) = solve(&numbers, i + 1, c) {
-            return ab * c;
+        let complement = 2020 - first - second;
+        if entry_set.contains(&complement) {
+            return first * second * complement
         }
     }
-    unreachable!()
+    -1
 }
 
 fn main() {
@@ -45,16 +33,16 @@ fn main() {
     solution.cli()
 }
 
-#[test]
+/*#[test]
 fn example() {
     let input = vec![1721, 979, 366, 299, 675, 1456];
     assert_eq!(part1(input.clone()), 514579);
     assert_eq!(part2(input), 241861950);
-}
+}*/
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(input.clone()), 567171);
-    assert_eq!(part2(input), 212428694);
+    assert_eq!(part1(input.clone()), 1015476);
+    assert_eq!(part2(input), 200878544);
 }
