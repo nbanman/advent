5ca7867997773aa422652b5b4f91ca1ee596bda5
Neil Banman <neil.banman@gmail.com>
2024-06-06 09:16:18 -0700

    initial

--- 2020/13.rs
+++ 2020/13.rs
@@ -1,45 +1,85 @@
 use advent::prelude::*;
 
-fn parse_input(input: &str) -> (i64, Vec<(i64, i64)>) {
-    let [ts, bus_ids] = input.lines().next_array().unwrap();
-    let bus_ids = bus_ids
-        .split(',')
-        .enumerate()
-        .filter_map(|(i, x)| match x {
-            "x" => None,
-            x => Some((i as i64, x.parse().unwrap())),
-        })
-        .collect();
-    (ts.parse().unwrap(), bus_ids)
+#[derive(Clone)]
+struct Bus {
+    id: i64,
+    offset: i64,
 }
 
-fn default_input() -> (i64, Vec<(i64, i64)>) {
+struct State {
+    time: i64,
+    bus_id: i64,
+}
+
+fn modular_inverse(ni: i64, modulus: i64) -> i64 {
+    iter::successors(Some(1), |i| Some(i + 1))
+        .find(|&n| (ni % modulus * n) % modulus == 1)
+        .unwrap()
+}
+
+fn crt(buses: Vec<Bus>) -> Bus {
+    let n = buses
+        .iter()
+        .fold(1, |acc, bus| acc * bus.id);
+    let big_phase: i64 = buses
+        .iter()
+        .map(|bus| {
+            let ni = n / bus.id;
+            bus.offset * ni * modular_inverse(ni, bus.id)
+        })
+        .sum();
+    Bus { id: n, offset: big_phase % n }
+}
+
+
+fn parse_input(input: &str) -> (i64, Vec<Bus>) {
+    if let Some((start, buses)) = input.split_once('\n') {
+        let start = start.parse().unwrap();
+        let buses: Vec<_> = buses
+            .split(',')
+            .enumerate()
+            .filter_map(|(idx, s)| {
+                if s == "x" {
+                    None
+                } else {
+                    let id = s.trim_end().parse().unwrap();
+                    Some(Bus { id, offset: idx as i64 })
+                }
+            })
+            .collect();
+        (start, buses)
+    } else {
+        panic!("Invalid input!")
+    }
+}
+
+fn default_input() -> (i64, Vec<Bus>) {
     parse_input(include_input!(2020 / 13))
 }
 
-fn part1((ts, bus_ids): (i64, Vec<(i64, i64)>)) -> i64 {
-    let mut t = ts;
-    loop {
-        t += 1;
-        for (_, id) in &bus_ids {
-            if t % id == 0 {
-                return id * (t - ts);
-            }
-        }
+fn part1((start, buses): (i64, Vec<Bus>)) -> i64 {
+    let time_sequence =
+        iter::successors(Some(State { time: start, bus_id: 0 }), |state| {
+            let time = state.time + 1;
+            let bus_id = buses.iter()
+                .find(|bus| time % bus.id == 0)
+                .map(|bus| bus.id)
+                .unwrap_or(0);
+            Some(State { time, bus_id })
+        });
+    if let Some(state) = time_sequence
+        .into_iter()
+        .find(|state| state.bus_id != 0) {
+
+        state.bus_id * (state.time - start)
+    } else {
+        panic!("No bus found!")
     }
 }
 
-fn part2((_, mut bus_ids): (i64, Vec<(i64, i64)>)) -> i64 {
-    bus_ids.sort_unstable_by_key(|(_, id)| *id);
-    let mut t = 0;
-    let mut dt = 1;
-    for (i, id) in bus_ids {
-        while (t + i) % id != 0 {
-            t += dt;
-        }
-        dt *= id;
-    }
-    t
+fn part2((_, buses): (i64, Vec<Bus>)) -> i64 {
+   let bus = crt(buses);
+    bus.id - bus.offset
 }
 
 fn main() {
@@ -57,6 +97,6 @@ fn example() {
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(input.clone()), 3246);
-    assert_eq!(part2(input), 1010182346291467);
+    assert_eq!(part1(input.clone()), 115);
+    assert_eq!(part2(input), 756261495958122);
 }
