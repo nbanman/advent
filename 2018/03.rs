16169856132ec036b6f01a1beb75eb39b2572095
Neil Banman <neil.banman@gmail.com>
2024-06-07 18:01:37 -0700

    initial

--- 2018/03.rs
+++ 2018/03.rs
@@ -1,79 +1,64 @@
-use std::ops::Range;
-
 use advent::prelude::*;
 
-fn parse_claim(input: &str) -> Claim {
-    let re = regex!(r"#(?P<id>\d+) @ (?P<x>\d+),(?P<y>\d+): (?P<dx>\d+)x(?P<dy>\d+)");
-    let caps = re.captures(input).unwrap();
-    let id = caps["id"].parse().unwrap();
-    let x = caps["x"].parse().unwrap();
-    let y = caps["y"].parse().unwrap();
-    let dx = caps["dx"].parse().unwrap();
-    let dy = caps["dy"].parse().unwrap();
-    let loc = vector![x, y];
-    let size = vector![dx, dy];
-    Claim { id, loc, size }
+#[derive(Clone, Debug)]
+struct Claim {
+    tl: Vector2,
+    br: Vector2,
+}
+fn parse_input(input: &str) -> (Vec<Claim>, Vec<i64>, i64) {
+    let claims: Vec<_> = input
+        .get_numbers()
+        .array_chunked()
+        .map(|[_, x, y, w, h]| {
+            Claim { tl: vector![x, y], br: vector![x + w - 1, y + h - 1] }
+        })
+        .collect();
+
+    let width = claims.iter().map(|claim| claim.br.x).max().unwrap() + 1;
+    let height = claims.iter().map(|claim| claim.br.y).max().unwrap() + 1;
+
+    let mut skein = vec![0i64; (width * height) as usize];
+    for claim in claims.iter() {
+        for y in claim.tl.y..=claim.br.y {
+            for x in claim.tl.x..=claim.br.x {
+                let idx = (y * width + x) as usize;
+                skein[idx] += 1;
+            }
+        }
+    }
+    (claims, skein, width)
 }
 
-fn parse_input(input: &str) -> Vec<Claim> {
-    input.lines().map(parse_claim).collect()
-}
-
-fn default_input() -> Vec<Claim> {
+fn default_input() -> (Vec<Claim>, Vec<i64>, i64) {
     parse_input(include_input!(2018 / 03))
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Claim {
-    id: i64,
-    loc: Vector2,
-    size: Vector2,
+fn part1((_, skein, _): (Vec<Claim>, Vec<i64>, i64)) -> usize {
+    skein.into_iter().filter(|it| it > &1).count()
 }
 
-fn xs(claim: &Claim) -> Range<i64> {
-    claim.loc.x..(claim.loc.x + claim.size.x)
-}
-
-fn ys(claim: &Claim) -> Range<i64> {
-    claim.loc.y..(claim.loc.y + claim.size.y)
-}
-
-fn squares(claim: &Claim) -> impl Iterator<Item = Vector2> + '_ {
-    xs(claim).flat_map(|x| ys(claim).map(move |y| vector![x, y]))
-}
-
-fn fabric(claims: &[Claim]) -> HashMap<Vector2, usize> {
-    let mut f: HashMap<Vector2, usize> = HashMap::new();
-    for claim in claims {
-        for sq in squares(claim) {
-            *f.entry(sq).or_default() += 1;
-        }
-    }
-    f
-}
-
-fn part1(claims: Vec<Claim>) -> usize {
-    fabric(&claims).values().filter(|&&n| n > 1).count()
-}
-
-fn part2(claims: Vec<Claim>) -> i64 {
-    let f = fabric(&claims);
-    claims
-        .iter()
-        .find_map(|claim| squares(claim).all(|sq| f[&sq] == 1).some(claim.id))
+fn part2((claims, skein, width): (Vec<Claim>, Vec<i64>, i64)) -> usize {
+    1 + claims.into_iter().enumerate()
+        .find(|(_, claim)| {
+            (claim.tl.y..=claim.br.y).cartesian_product(claim.tl.x..=claim.br.x)
+                .all(|(y, x)| {
+                    let pos = y * width + x;
+                    skein[pos as usize] == 1
+                })
+        })
         .unwrap()
+        .0
 }
+
 
 fn main() {
     let solution = advent::new(default_input).part(part1).part(part2).build();
     solution.cli()
 }
-
 #[test]
 fn example() {
     let input = parse_input(
-        "\
-#1 @ 1,3: 4x4
+        "#1 @ 1,3: 4x4
 #2 @ 3,1: 4x4
 #3 @ 5,5: 2x2",
     );
@@ -84,6 +69,6 @@ fn example() {
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(input.clone()), 117948);
-    assert_eq!(part2(input), 567);
+    assert_eq!(part1(input.clone()), 110891);
+    assert_eq!(part2(input), 297);
 }
