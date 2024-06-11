c59b80f642e8c573d3ed645a0b16ad0e29fb6397
Neil Banman <neil.banman@gmail.com>
2024-06-11 11:02:45 -0700

    initial

---
+++ 2017/02.rs
@@ -0,0 +1,70 @@
+use advent::prelude::*;
+
+fn parse_input(input: &str) -> Vec<Vec<usize>> {
+    input.lines().map(|line| get_numbers(line)).collect()
+}
+
+fn default_input() -> Vec<Vec<usize>> {
+    parse_input(include_input!(2017 / 02))
+}
+
+fn solve<F>(spreadsheet: Vec<Vec<usize>>, line_operation: F) -> usize
+where
+    F: Fn(Vec<usize>) -> usize,
+{
+    spreadsheet.into_iter().map(|row| line_operation(row)).sum()
+}
+fn part1(spreadsheet: Vec<Vec<usize>>) -> usize {
+    solve(spreadsheet, |row| {
+        let (min, max) = min_max(&row);
+        max - min
+    })
+}
+
+fn part2(spreadsheet: Vec<Vec<usize>>) -> usize {
+    solve(spreadsheet, |row| {
+        let mut sum = 0;
+        for i in 0..row.len() - 1 {
+            for j in &row[i + 1..row.len()] {
+                let i = row[i];
+                let bind = &[i, *j];
+                let (smaller, larger) = min_max(bind);
+                if larger % smaller == 0 {
+                    sum += larger / smaller
+                }
+            }
+        }
+        sum
+    })
+}
+
+fn main() {
+    let solution = advent::new(default_input).part(part1).part(part2).build();
+    solution.cli()
+}
+
+#[test]
+fn example1() {
+    let input = parse_input(
+        "5 1 9 5
+7 5 3
+2 4 6 8",
+    );
+    assert_eq!(part1(input), 18)
+}
+
+#[test]
+fn example2() {
+    let input = parse_input(
+        "5 9 2 8
+9 4 7 3
+3 8 6 5",
+    );
+    assert_eq!(part2(input), 9)
+}
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(input.clone()), 45972);
+    assert_eq!(part2(input), 326);
+}
---
+++ 2017/03.rs
@@ -0,0 +1,70 @@
+use advent::prelude::*;
+
+const DIRECTIONS: [Vector<i64, 2>; 4] = [
+    Vector2::new(0, -1), // N
+    Vector2::new(1, 0), // E
+    Vector2::new(0, 1), // S
+    Vector2::new(-1, 0), // W
+];
+
+const DIAGONALS: [Vector<i64, 2>; 8] = [
+    Vector2::new(-1, -1),
+    Vector2::new(0, -1),
+    Vector2::new(1, -1),
+    Vector2::new(1, 0),
+    Vector2::new(1, 1),
+    Vector2::new(0, 1),
+    Vector2::new(-1, 1),
+    Vector2::new(-1, 0),
+];
+fn parse_input(input: &str) -> usize {
+    input.trim_end().parse().unwrap()
+}
+
+fn default_input() -> usize {
+    parse_input(include_input!(2017 / 03))
+}
+
+fn part1(input: usize) -> usize {
+    let square_root = (input as f64).sqrt().ceil() as usize;
+    let square_root = if square_root & 1 == 0 { square_root + 1 } else { square_root };
+    let furthest = (square_root / 2) * 2;
+    let br = square_root.pow(2);
+    let diff = (br - input) % furthest;
+    furthest - min(diff, furthest / 2) + max(0, diff - furthest / 2)
+}
+
+fn part2(input: usize) -> usize {
+    let mut dir = 2i8;
+    let mut pos = Vector2::zero();
+    let mut vel = 0i64;
+    let mut space = HashMap::new();
+    space.insert(pos.clone(), 1i64);
+    loop {
+        dir = (dir - 1).rem_euclid(DIRECTIONS.len() as i8);
+        if dir == 1 || dir == 3 { vel += 1; }
+        for _ in 1..=vel {
+            pos = pos + DIRECTIONS[dir as usize];
+            let square_val = DIAGONALS.iter()
+                .map(|&dir| {
+                    let neighbor = pos + dir;
+                    space.get(&neighbor).unwrap_or(&0)
+                })
+                .sum::<i64>() as usize;
+            if square_val > input { return square_val }
+            space.insert(pos, square_val as i64);
+        }
+    }
+}
+
+fn main() {
+    let solution = advent::new(default_input).part(part1).part(part2).build();
+    solution.cli()
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(input.clone()), 552);
+    assert_eq!(part2(input), 330785);
+}
